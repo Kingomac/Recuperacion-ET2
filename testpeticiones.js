e689fbfde29c1cb5d0a6cdfd818a91009cef5198
Kingomac <kingomacyt@gmail.com>
2022-12-07 13:41:55 +0100

    cambios xd

--- testpeticiones.js
+++ testpeticiones.js
@@ -5,45 +5,50 @@ const URL_BACK = "http://193.147.87.202/Back/index.php";
 	funcion principal para iniciar la tabla e invocar la llamada a la funcion que lanza los test a la promesa
 */
 
-function realizarTestPeticiones() {
-  $("#tituloresultados").html("");
-  titulotabla =
-    "<tr><th>Num. Prueba</th><th>Controlador</th><th>Accion</th><th>Atributos</th><th>Obtenido</th><th>Esperado</th><th>Correcta</th></tr>";
-  $("#tituloresultados").append(titulotabla);
-
+async function realizarTestPeticiones() {
   const tbody = document.getElementById("datosresultados");
-  for (const prueba of Object.entries(test)) {
-    const tdObtenido = document.createElement("td");
-    tdObtenido.append(document.createElement("progress"));
-    const tdOK = document.createElement("td");
-    tdOK.append(document.createElement("progress"));
-    const tr = crearTR(
-      prueba[0],
-      prueba[1].valores.controlador,
-      prueba[1].valores.action,
-      getAtributos(prueba[1].valores),
-      tdObtenido,
-      prueba[1].condicion.valor,
-      tdOK
-    );
-    tbody.append(tr);
-    peticionBack(new URLSearchParams(prueba[1].valores))
-      .then((resultado) => {
-        tdObtenido.textContent = resultado;
-        if (resultado == prueba[1].condicion.valor) {
-          tdOK.textContent = "SI";
-          tr.style.backgroundColor = "#56e359";
-        } else {
-          tdOK.textContent = "NO";
-          tr.style.backgroundColor = "#ff3838";
-        }
-      })
-      .catch((err) => {
-        tdObtenido.textContent = err;
-        tdOK.textContent = "NO";
-        tr.style.backgroundColor = "#ff70f1";
-      });
+  const arrPruebas = Object.entries(test);
+
+  const trBarraCarga = document.getElementById("tr-barra-carga");
+  const barraCarga = trBarraCarga.querySelector("progress");
+  barraCarga.max = arrPruebas.length;
+  barraCarga.value = 0;
+
+  for (const prueba of arrPruebas) {
+    let tr;
+    try {
+      const resultado = await peticionBack(
+        new URLSearchParams(prueba[1].valores)
+      );
+      const correcta = resultado == prueba[1].condicion.valor;
+      tr = crearTR(
+        prueba[0],
+        prueba[1].valores.controlador,
+        prueba[1].valores.action,
+        getAtributos(prueba[1].valores),
+        resultado,
+        prueba[1].condicion.valor,
+        correcta ? "SI" : "NO"
+      );
+      tr.style.backgroundColor = correcta ? "#56e359" : "#ff3838";
+    } catch (err) {
+      tr = crearTR(
+        prueba[0],
+        prueba[1].valores.controlador,
+        prueba[1].valores.action,
+        getAtributos(prueba[1].valores),
+        err,
+        prueba[1].condicion.valor,
+        "NO"
+      );
+      tr.style.backgroundColor = "#ff70f1";
+    } finally {
+      tbody.append(tr);
+      barraCarga.value++;
+    }
   }
+
+  trBarraCarga.remove();
 }
 
 /**
@@ -87,13 +92,9 @@ function peticionBack(datos) {
 function crearTR(...nodos) {
   const tr = document.createElement("tr");
   for (const n of nodos) {
-    if (n instanceof HTMLTableCellElement) {
-      tr.append(n);
-    } else {
-      const td = document.createElement("td");
-      td.append(n);
-      tr.append(td);
-    }
+    const td = document.createElement("td");
+    td.append(n);
+    tr.append(td);
   }
   return tr;
 }
